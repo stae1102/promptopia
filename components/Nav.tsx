2c177b72c38836f0b7ad0b752c8da5cc6e07c5ee
Seongtae <stae1102@gmail.com>
2023-05-12 07:20:09 +0900

    feat: Show nav for mobile

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,6 +10,8 @@ const Nav = () => {
 
   const [providers, setProviders] = useState(null);
 
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
   useEffect(() => {
     const setProviders = async () => {
       const response = await getProviders();
@@ -54,6 +56,67 @@ const Nav = () => {
                 alt='Profile'
               />
             </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? (
+          <div className='flex'>
+            <Image
+              src='/assets/images/logo.svg'
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='Profile'
+              onClick={() =>
+                setToggleDropdown((prevToggleDropdown) => !prevToggleDropdown)
+              }
+            />
+
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <Link
+                  href='/profile'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
