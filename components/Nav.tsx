857202bf9de67b27dcb03e87a11aad6c220bf8de
Seongtae <stae1102@gmail.com>
2023-05-12 07:08:20 +0900

    feat: Show create post and signout button

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
@@ -16,7 +18,35 @@ const Nav = () => {
           height={30}
           className='object-contain'
         />
+        <p className='logo_text'>Promptopia</p>
       </Link>
+
+      {/* Desktop Navigation */}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
+            </Link>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='Profile'
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
