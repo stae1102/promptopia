0e2bfaa852b40e1f0684e0a9ea857c05e9135ae2
Seongtae <stae1102@gmail.com>
2023-05-15 23:20:47 +0900

    feat: Get prompt card by using fetch

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,10 +4,36 @@ import { useState, useEffect, ChangeEvent } from 'react';
 
 import PromptCard from './PromptCard';
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => {
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />;
+      })}
+    </div>
+  );
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e: ChangeEvent) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
     <section className='feed'>
@@ -22,7 +48,7 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={[]} handleTagClick={() => {}} />
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
