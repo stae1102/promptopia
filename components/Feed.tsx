ec82efa8e890320fe703cf37409d0d235292e35e
Seongtae <stae1102@gmail.com>
2023-05-15 23:01:46 +0900

    feat: Create Feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,30 @@
+'use client';
+
+import { useState, useEffect, ChangeEvent } from 'react';
+
+import PromptCard from './PromptCard';
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e: ChangeEvent) => {};
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
