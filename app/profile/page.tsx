b554efe9d447073c45f0d0b3ddf9c92d581eae00
Seongtae <stae1102@gmail.com>
2023-05-18 07:18:00 +0900

    feat: Create profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,26 @@
-const Profile = () => {
-  return <div>Profile</div>;
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/Profile';
+
+const MyProfile = () => {
+  const [data, setData] = useState([]);
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page'
+      data={data}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    ></Profile>
+  );
 };
 
-export default Profile;
+export default MyProfile;
