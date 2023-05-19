edc0cfdba138863e026be1885de6c3c87a7879aa
Seongtae <stae1102@gmail.com>
2023-05-19 23:39:37 +0900

    feat: Create PATCH prompt

--- app/api/prompt/[id]/route.ts
+++ app/api/prompt/[id]/route.ts
@@ -18,3 +18,30 @@ export const GET = async (request: Request, { params }) => {
     });
   }
 };
+
+// PATCH
+export const PATCH = async (request: Request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt)
+      return new Response('Prompt not found', { status: 404 });
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response('Failed to update prompt', {
+      status: 500,
+    });
+  }
+};
+
